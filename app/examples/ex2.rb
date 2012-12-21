696c67068caa87b94de6a3986b1093b2133b36dd
Robert Moore <romoore@cs.rutgers.edu>
2012-12-21 14:35:27 -0500

    Example 2

---
+++ app/examples/ex2.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+#
+# http://ruby.learncodethehardway.org/book/ex2.html
+#
+
+puts "I could have code like this." # and the comment after is ignored
+
+# puts "This won't run."
+puts "This will run."
