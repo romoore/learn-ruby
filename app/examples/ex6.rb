001816bed1df1b30826af0ab0f70bd9dd01042b1
Robert Moore <romoore@cs.rutgers.edu>
2012-12-21 20:52:57 -0500

    Exercise 6.

---
+++ app/examples/ex6.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+#
+# http://ruby.learncodethehardway.org/book/ex6.html
+#
+
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = true
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of. . ."
+e = "a string with a right side."
+
+puts w + e
+
