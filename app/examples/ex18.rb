15ad53e6d76671eae79953d190322b7a99ea4260
Robert Moore <romoore@cs.rutgers.edu>
2012-12-26 12:09:29 -0500

    Exercise 18.

---
+++ app/examples/ex18.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+#
+# http://ruby.learncodethehardway.org/book/ex18.html
+#
+
+# this one is like your scripts with argv
+def puts_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def puts_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def puts_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+def puts_none()
+  puts "I got nothin."
+end
+
+puts_two("Zed","Shaw")
+puts_two_again("Zed", "Shaw")
+puts_one("First!")
+puts_none()
