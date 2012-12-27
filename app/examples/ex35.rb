e0b9527075d9384745a6f3890e8b9ed0d1de1091
Robert Moore <romoore@cs.rutgers.edu>
2012-12-27 08:43:59 -0500

    Adjusting gold amount-checking code.

--- app/examples/ex35.rb
+++ app/examples/ex35.rb
@@ -10,7 +10,8 @@ def gold_room()
   puts "This room is full of gold.  How much do you take?"
 
   prompt; next_move = gets.chomp
-  if next_move.include? "0" or next_move.include? "1"
+  how_much = next_move.to_i()
+  if how_much > 0
     how_much = next_move.to_i()
   else
     dead("Man, learn to type a number.")
